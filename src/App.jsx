66f1c6847169a021805d695421c393e3b47bf209
muxlisa-maxmudova <akobirurozov22@gmail.com>
2025-04-11 17:49:37 +0500

    regular react_18 code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-const App = () => {
-
-
+import React, {useState} from 'react'
+import {updateNameInDB} from "./api.js";
+function App(){
+    const [input, setInput] = useState('')
+    const [name, setName] = useState(
+        ()=>JSON.parse(localStorage.getItem('name'))||'Anonymous user'
+    )
+    function handleChange(e){
+        setInput(e.target.value)
+    }
+   async function handleSubmit(e){
+        e.preventDefault()
+        try {
+            const newName = updateNameInDB(input)
+            setName(newName)
+            setInput('')
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
     return (
         <div>
-
+            <p className={'username'}>
+                Current User: <span>{name}</span>
+            </p>
+            <form onSubmit={handleSubmit}>
+                <input
+                onChange={handleChange}
+                type = "text"
+                required/>
+                <button type="submit">
+                    Update User
+                </button>
+            </form>
         </div>
     )
 }
